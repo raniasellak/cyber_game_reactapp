78bbc338ac81d20172817cfe59c7692aaf8218db
Rania sellak <git>
2026-09-20 18:05:06 +0100

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ← ajoute
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // ← ajoute tailwindcss()
+  base: '/cyber_game_reactapp/',
+  plugins: [react(), tailwindcss()],
 })
